1f019f30818073938d1d3b99d219099e37cd0e42
david <d.albrecht@seznam.cz>
2022-03-07 10:48:37 +0100

    + Spring Boot to pom, json only so far + Main class and beans needed for rest client check if this builds.

---
+++ src/test/java/StarWarsSearch.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+public class StarWarsSearch {
+
+    @Bean
+    RestTemplateBuilder restTemplateBuilder() {
+        return new RestTemplateBuilder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsSearch.class, args);
+    }
+}
