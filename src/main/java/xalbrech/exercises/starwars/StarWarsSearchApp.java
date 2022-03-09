aeaf06183cb7f361089284adaa2702b9bca07144
david <d.albrecht@seznam.cz>
2022-03-09 10:04:35 +0100

    + ApiCrawler as bean + temporary test code to search for a planet.

--- src/main/java/xalbrech/exercises/starwars/StarWarsSearchApp.java
+++ src/main/java/xalbrech/exercises/starwars/StarWarsSearchApp.java
@@ -2,11 +2,14 @@ package xalbrech.exercises.starwars;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import xalbrech.exercises.starwars.crawler.ApiCrawler;
+import xalbrech.exercises.starwars.index.SearchIndex;
 
 @SpringBootConfiguration
 public class StarWarsSearchApp {
@@ -26,6 +29,19 @@ public class StarWarsSearchApp {
     @Bean
     public SearchIndex searchIndex() {
         return new SearchIndex();
+    }
+
+    @Bean
+    public ApiCrawler apiCrawler() {
+        return new ApiCrawler();
+    }
+
+    @Bean
+    public CommandLineRunner run(ApiCrawler apiCrawler, SearchIndex searchIndex) throws Exception {
+        return args -> {
+            apiCrawler.crawl();
+            log.info("{}", searchIndex.search("Tatooine"));
+        };
     }
 
     public static void main(String[] args) {
