2aaf38710593add91d0614341b3b92c879c76217
david <d.albrecht@seznam.cz>
2022-03-16 11:30:19 +0100

    Refactoring: - Initialization of search index (ApiCrawler.crawl) is now via a new spearate InitializingBean (so the index gets populated before search endpoint is exposed). Rewritten using MockMvc.

--- src/test/java/xalbrech/exercises/starwars/controller/SearchControllerTest.java
+++ src/test/java/xalbrech/exercises/starwars/controller/SearchControllerTest.java
@@ -3,42 +3,54 @@ package xalbrech.exercises.starwars.controller;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import xalbrech.exercises.starwars.index.SearchIndex;
 
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = SearchController.class)
+@EnableWebMvc
+@AutoConfigureMockMvc
 class SearchControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @MockBean
     private SearchIndex searchIndex;
-
-    @Autowired
-    private SearchController searchController;
 
     @Mock
     private SearchResult searchResult;
 
     @Test
-    public void searchReachesSearchIndex() throws MalformedURLException {
+    public void searchReachesSearchIndex() throws Exception {
         Collection<URI> searchIndexReply = Arrays.asList(URI.create("http://test/url"));
         when(searchIndex.search("test")).thenReturn(searchIndexReply);
 
-        SearchResult result = searchController.search("test");
-        verify(searchIndex).search("test");
+        mockMvc.perform(get("/starwars?term=test"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.searchTerm", is("test")))
+                .andExpect(jsonPath("$.searchResults", contains("http://test/url")));
 
-        assertEquals(searchIndexReply, result.getSearchResults());
-        assertEquals("test", result.getSearchTerm());
+        verify(searchIndex).search("test");
     }
 
 }
