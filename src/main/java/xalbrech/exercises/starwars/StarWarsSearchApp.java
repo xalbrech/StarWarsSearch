d959e3fdc954d9fa7e4570fe2145fd2eec78c4e4
David Albrecht <david.albrecht@persoo.com>
2022-04-24 22:10:54 +0200

    minor - imports tidy up

--- src/main/java/xalbrech/exercises/starwars/StarWarsSearchApp.java
+++ src/main/java/xalbrech/exercises/starwars/StarWarsSearchApp.java
@@ -1,16 +1,10 @@
 package xalbrech.exercises.starwars;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.web.client.RestTemplate;
-import xalbrech.exercises.starwars.crawler.ApiCrawler;
-import xalbrech.exercises.starwars.index.SearchIndex;
 
 /**
  * Main app class - actual main method to start the app, definition of necessary beans.
--- src/test/java/xalbrech/exercises/starwars/controller/SearchControllerTest.java
+++ src/test/java/xalbrech/exercises/starwars/controller/SearchControllerTest.java
@@ -1,23 +1,5 @@
 package xalbrech.exercises.starwars.controller;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import xalbrech.exercises.starwars.index.SearchIndex;
-
-import java.net.URI;
-import java.util.Arrays;
-import java.util.Collection;
-
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
@@ -25,6 +7,21 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import xalbrech.exercises.starwars.index.SearchIndex;
 
 @SpringBootTest(classes = SearchController.class)
 @EnableWebMvc
